6b0b1130773756c177e33a34d5bd9c3a2fc485a4
Douglas Trindade <douglastrindad@hotmail.com>
2022-01-20 18:58:23 -0300

    desafio-modal

--- desafio.js
+++ desafio.js
@@ -1,41 +1,84 @@
-fetch("dados.json")
-  .then((response) => response.json())
-  .then((json) => {
-    localStorage.setItem("posts", JSON.stringify(json));
-    renderTable(json);
-  });
-
-const localStorageTransactions = JSON.parse(localStorage.getItem("posts"));
-
 const table = document.getElementById("t-body");
 const input = document.getElementById("pesquisar");
 const buttom = document.getElementById("btn");
 const dropdonwBtn = document.querySelectorAll(".dropdown-item");
 const openModal1 = document.getElementById("modal");
 
-buttom.addEventListener("click", function () {});
+const init = () => {
+  if (localStorageGet()) {
+     renderTable(JSON.parse(localStorage.getItem("posts")));
+     return
+  }
+
+  getPosts();
+};
 
 
+const getPosts = async() => {
+  const posts = await fetch("https://jsonplaceholder.typicode.com/posts").then(res => res.json());
+  
+  localStorage.setItem("posts", JSON.stringify(posts));
+  renderTable(posts);
+};
+
+const localStorageGet = () => {
+  return JSON.parse(localStorage.getItem("posts"));
+};
+
+buttom.addEventListener("click", () => {
+  input.value = '';
+  getPosts();
+
+});
 // DELETE
 const deletePost = (value) => {
   document.getElementById("delete").addEventListener("click", () => {
-    const deletID = JSON.parse(localStorage.getItem("posts")).filter((dado) => {
+    const deletID = localStorageGet().filter((dado) => {
       return dado.id != value;
     });
-
     localStorage.setItem("posts", JSON.stringify(deletID));
-    $('#staticBackdrop').modal('hide')
 
+    $("#staticBackdrop").modal("hide");
+    renderTable(localStorageGet());
 
-
-    renderTable(JSON.parse(localStorage.getItem("posts")));
-    console.log(deletID);
+    input.value = '';
   });
- 
+};
+
+const editarPost = postId =>{
+
+  const titulo = document.getElementById("titulo");
+  const descricao = document.getElementById("descrição");
+  const salvar = document.getElementById("salvar");
+  const edit = localStorageGet().find(dado => {
+    return dado.id === postId
+
+  });
+
+  titulo.value = edit.title;
+  descricao.value = edit.body;
+
+  console.log(edit)
+
+  salvar.addEventListener('click', () =>{
+    console.log(salvar)
+    const savePost = localStorageGet().map(dado =>{
+if( dado.id === postId){
+return ;
+
+}
+
+
+});
+
+  $("#staticBackdrop").modal("hide");
+  });
 
 };
 
+
 const renderTable = (localStorage) => {
+  console.log(localStorage)
   let html = "";
   localStorage.forEach((response) => {
     html += `
@@ -46,7 +89,7 @@ const renderTable = (localStorage) => {
             <div class="dropdown">
               <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-expanded="false"> Ações</a>
             <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                <a class="dropdown-item"  data-toggle="modal" data-target="#exampleModal" onclink="editarPost(${response.id})">Editar</a>
+                <a class="dropdown-item"  data-toggle="modal" data-target="#exampleModal" onclick="editarPost(${response.id})">Editar</a>
                 <a class="dropdown-item"  data-toggle="modal" data-target="#staticBackdrop" onclick="deletePost(${response.id})">Remover</a>
               </div>
             </div>
@@ -58,11 +101,10 @@ const renderTable = (localStorage) => {
 };
 
 const filterInput = () => {
-  const filterTitle = localStorageTransactions.filter((dado) => {
+  const filterTitle = localStorageGet().filter((dado) => {
     if (!input.value) {
       return true;
     }
-
     if (
       input.value &&
       dado.title.includes(
@@ -77,5 +119,8 @@ const filterInput = () => {
     return false;
   });
 
+
   renderTable(filterTitle);
 };
+
+init();
