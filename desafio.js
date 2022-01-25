53c14bf4384a3e3948a8ee4079174b761734004c
Douglas Trindade <douglastrindad@hotmail.com>
2022-01-25 17:25:36 -0300

    first commit

--- desafio.js
+++ desafio.js
@@ -2,23 +2,35 @@ const table = document.getElementById("t-body");
 const input = document.getElementById("pesquisar");
 const buttom = document.getElementById("btn");
 const dropdonwBtn = document.querySelectorAll(".dropdown-item");
-const openModal1 = document.getElementById("modal");
+const openModal = document.getElementById("modal");
+let loader = document.querySelector(".loader");
+const adicionarPost = document.getElementById("exampleModalLabell");
+ console.log(adicionarPost)
+const renderQuery = () =>
+  $(document).ready(() => {
+    console.log(123)
+    $("#datatable").DataTable();
+  });
 
 const init = () => {
-  if (localStorageGet()) {
-     renderTable(JSON.parse(localStorage.getItem("posts")));
-     return
-  }
+  renderQuery();
+  const posts = localStorageGet();
 
+  if (posts) {
+    console.log('')
+    renderTable(posts);
+    return;
+  }
   getPosts();
 };
 
-
-const getPosts = async() => {
-  const posts = await fetch("https://jsonplaceholder.typicode.com/posts").then(res => res.json());
-  
-  localStorage.setItem("posts", JSON.stringify(posts));
-  renderTable(posts);
+const getPosts = () => {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((json) => {
+      localStorage.setItem("posts", JSON.stringify(json));
+      renderTable(json);
+    });
 };
 
 const localStorageGet = () => {
@@ -26,71 +38,79 @@ const localStorageGet = () => {
 };
 
 buttom.addEventListener("click", () => {
-  input.value = '';
   getPosts();
-
 });
-// DELETE
+
 const deletePost = (value) => {
-  document.getElementById("delete").addEventListener("click", () => {
-    const deletID = localStorageGet().filter((dado) => {
-      return dado.id != value;
-    });
-    localStorage.setItem("posts", JSON.stringify(deletID));
-
+  document.getElementById("delete-post").addEventListener("click", () => {
+    loader.style.visibility = "visible";
     $("#staticBackdrop").modal("hide");
-    renderTable(localStorageGet());
+    setTimeout(function () {
+      const posts = localStorageGet();
 
-    input.value = '';
+      const deletID = posts.filter((dado) => {
+        return dado.id !== value;
+      });
+
+      localStorage.setItem("posts", JSON.stringify(deletID));
+
+      renderTable(localStorageGet());
+      loader.style.visibility = "hidden";
+    }, 800);
   });
 };
 
-const editarPost = postId =>{
-
+const editarPost = (postId) => {
   const titulo = document.getElementById("titulo");
   const descricao = document.getElementById("descrição");
   const salvar = document.getElementById("salvar");
-  const edit = localStorageGet().find(dado => {
-    return dado.id === postId
+  let posts = localStorageGet();
 
+  let edit = posts.find((dado) => {
+    return dado.id === postId;
   });
 
   titulo.value = edit.title;
   descricao.value = edit.body;
 
-  console.log(edit)
+  salvar.addEventListener("click", () => {
+    if (!titulo.value) {
+      alert("Campo vazio");
+      return;
+    }
+    if (!descricao.value) {
+      alert("Campo vazio");
+      return;
+    }
 
-  salvar.addEventListener('click', () =>{
-    console.log(salvar)
-    const savePost = localStorageGet().map(dado =>{
-if( dado.id === postId){
-return ;
+    loader.style.visibility = "visible";
+    $("#exampleModal").modal("hide");
 
-}
-
-
-});
-
-  $("#staticBackdrop").modal("hide");
+    setTimeout(function () {
+      edit.title = titulo.value;
+      edit.body = descricao.value;
+      posts[postId - 1] = edit;
+      localStorage.setItem("posts", JSON.stringify(posts));
+      loader.style.visibility = "hidden";
+      renderTable(posts);
+    }, 800);
   });
-
 };
 
-
-const renderTable = (localStorage) => {
-  console.log(localStorage)
+const renderTable = (dados) => {
   let html = "";
-  localStorage.forEach((response) => {
+
+  dados.forEach((dado) => {
     html += `
         <tr>
-          <td>${response.id}</td>
-          <td>${response.title}</td>
+          <td>${dado.id}</td>
+          <td>${dado.title}</td>
           <td class="text-end">
             <div class="dropdown">
               <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-expanded="false"> Ações</a>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                <a class="dropdown-item"  data-toggle="modal" data-target="#exampleModal" onclick="editarPost(${response.id})">Editar</a>
-                <a class="dropdown-item"  data-toggle="modal" data-target="#staticBackdrop" onclick="deletePost(${response.id})">Remover</a>
+              <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <a class="dropdown-item"  data-toggle="modal" data-target="#exampleModal" onclick="editarPost(${dado.id})">Editar</a>
+                <a class="dropdown-item"  data-toggle="modal" data-target="#staticBackdrop" onclick="deletePost(${dado.id})">Remover</a>
               </div>
             </div>
           </td>
@@ -100,27 +120,26 @@ const renderTable = (localStorage) => {
   table.innerHTML = html;
 };
 
-const filterInput = () => {
-  const filterTitle = localStorageGet().filter((dado) => {
-    if (!input.value) {
-      return true;
-    }
-    if (
-      input.value &&
-      dado.title.includes(
-        input.value
-          .toLowerCase()
-          .normalize("NFD")
-          .replace(/[\u0300-\u036f]/g, "")
-      )
-    ) {
-      return true;
-    }
-    return false;
-  });
+// const filterInput = () => {
+//   const filterTitle = localStorageGet().filter((dado) => {
+//     if (!input.value) {
+//       return true;
+//     }
+//     if (
+//       input.value &&
+//       dado.title.includes(
+//         input.value
+//           .toLowerCase()
+//           .normalize("NFD")
+//           .replace(/[\u0300-\u036f]/g, "")
+//       )
+//     ) {
+//       return true;
+//     }
+//     return false;
+//   });
 
-
-  renderTable(filterTitle);
-};
+//   renderTable(filterTitle);
+// };
 
 init();
